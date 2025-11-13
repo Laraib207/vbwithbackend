07da322555ddc434d2f36b8f4ac294e068965a58
Laraib207 <laraibsiddiqui10ki@gmail.com>
2025-11-13 15:22:04 +0530

    SEOCONTENT

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 export const metadata = {
   title: "Veer Bharat | Best Soyabean Oil | Kachi Ghani Oil",
-  description: "Veer Bharat Premium is the best Soyabean Oil in Ghaziabad. Trusted Indian brand Kachi Ghani Oil quality cooking oil, rice bran oil in Noida. Call 9266328444"
+  description: "Veer Bharat Premium is the best Soyabean Oil in Ghaziabad. Trusted Indian brand Kachi Ghani Oil quality cooking oil, rice bran oil in Noida. Call 9266328444",
+  other: {
+    'google-site-verification': 'r98v6O5kAtTe-6BdgRB11PQgQTopFn7x464rMPuWkIQ',
+  },
 };
 
 import "./globals.css";
